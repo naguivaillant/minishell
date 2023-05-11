c89a3847879f08e59e136c8b5937231ee3e78179
mrabourd <133120742+mrabourd@users.noreply.github.com>
2023-05-11 19:41:00 +0200

    Add files via upload

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mrabourd <mrabourd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 16:59:43 by mrabourd          #+#    #+#             */
-/*   Updated: 2023/05/04 14:57:33 by mrabourd         ###   ########.fr       */
+/*   Updated: 2023/05/11 13:19:52 by mrabourd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ t_list	*ft_lstnew(void *content)
 	new->content = content;
 	new->full = 0;
 	new->printed = 0;
+	new->type = 0;
 	return (new);
 }
