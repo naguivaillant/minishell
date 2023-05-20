6b0ff2a2aaff9134037287e0fe8c43d2ac26e623
mrabourd <133120742+mrabourd@users.noreply.github.com>
2023-05-20 17:45:37 +0200

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,72 +6,21 @@
 /*   By: mrabourd <mrabourd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 18:07:16 by mrabourd          #+#    #+#             */
-/*   Updated: 2023/05/11 19:27:09 by mrabourd         ###   ########.fr       */
+/*   Updated: 2023/05/20 17:42:27 by mrabourd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-void	parse_cmd(t_data *data)
-{
-	int	i;
-	int	j;
 
-	i = 0;
-	j = 0;
-	while (data->cmd[i])
-	{
-		if (data->cmd[i][j] == 'e')
-		{
-			j++;
-			if (data->cmd[i][j] == 'n')
-			{
-				j++;
-				if (data->cmd[i][j] == 'v')
-				{
-					print_env(data);
-					return ;
-				}
-			}
-			else if (data->cmd[i][j] == 'c')
-			{
-				j++;
-				if (data->cmd[i][j] == 'h')
-				{
-					j++;
-					if (data->cmd[i][j] == 'o')
-					{
-						mini_echo(data);
-						return ;
-					}
-				}
-			}
-			else if (data->cmd[i][j] == 'x')
-			{
-				j++;
-				if (data->cmd[i][j] == 'p')
-				{
-					j++;
-					if (data->cmd[i][j] == 'o')
-					{
-						builtin_export(data);
-						return ;
-					}
-				}
-			}
-		}
-	}
-}
-*/
 void	print_all(t_data *data)
 {
 	t_list	*tmp;
 
-	tmp = data->env;
+	tmp = data->token_list;
 	printf("----START ALL-----\n");
 	while (tmp != NULL)
 	{
-		printf("%s\n", (char *)tmp->content);
+		printf("%s\n", tmp->content);
 		// printf("%d\n", tmp->printed);
 		tmp = tmp->next;
 	}
@@ -79,40 +28,33 @@ void	print_all(t_data *data)
 	printf("----END ALL-----\n");
 }
 
-void	fill_cmd_list(t_data *data)
-{
-	int		i;
-	int		res;
-	t_list	*new;
-
-	i = 0;
-	res = 0;
-	new = NULL;
-	while (data->cmd[i])
-	{
-		new = ft_lstnew(data->cmd[i]);
-		res = ft_lstadd_back(&data->cmd_list, new);
-		if (res == 1)
-			exit_all(data);
-		i++;
-	}
-	// print_all(data);
-}
-
 void	exec_cmd(t_data *data)
 {
 	t_list	*tmp;
 
-	tmp = data->cmd_list;
+	tmp = data->token_list;
 	while (tmp != NULL)
 	{
-		if (ft_strncmp((char *)tmp->content, "export", ft_strlen("export")) == 0)
-			builtin_export(data);
-		if (ft_strncmp((char *)tmp->content, "env", ft_strlen("env")) == 0)
+		// if (ft_strncmp((char *)tmp->content, "export", ft_strlen(tmp->content)) == 0)
+		// 	builtin_export(data, tmp);
+		if (ft_strncmp((char *)tmp->content, "env", ft_strlen(tmp->content)) == 0)
 			print_env(data);
-		if (ft_strncmp((char *)tmp->content, "echo", ft_strlen("echo")) == 0)
+		if (ft_strncmp((char *)tmp->content, "echo", ft_strlen(tmp->content)) == 0)
 			mini_echo(data);
+		if (ft_strncmp((char *)tmp->content, "unset", ft_strlen(tmp->content)) == 0)
+			builtin_unset(data);
 		tmp = tmp->next;
+	}
+}
+
+void	ft_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
 }
 
@@ -122,23 +64,37 @@ int	main(int argc, char **argv, char **env)
 	(void)argv;
 
 	ft_bzero(&data, sizeof(data));
-	ft_bzero(&data.cmd_list, sizeof(data.cmd_list));
+	ft_bzero(&data.token_list, sizeof(data.token_list));
 	ft_bzero(&data.env, sizeof(data.env));
 	// printf("&data.env %s\n", (char *)&data.env->content);
 	if (!env || env == NULL || argc != 1)
-		exit(EXIT_FAILURE);
+		exit_all(&data, 1);
+	parse_path(env, &data);
+	// fill_env_list(env, &data);
 	while (1)
 	{
+		signal(SIGINT, ft_handler);
+		signal(SIGQUIT, SIG_IGN);
 		data.input = readline("Minishell>");
+		if (!data.input)
+		{
+			exit_all(&data, 0);
+		}
 		add_history(data.input);
-		parse_path(env, &data);
-		fill_env_list(env, &data);
-		data.cmd = ft_split(data.input, ' ');
-		fill_cmd_list(&data);
-		exec_cmd(&data);
-		// parse_cmd(&data);/*pourtester mais faudra utiliser la listechainee*/
-		ft_lstclear(&data.cmd_list, free);
+		parse_cmd(&data);
+	//	execution(&data);a faire
 	}
-	exit_all(&data);
 	return (0);
 }
+
+/*
+OLD :
+		data.input = readline("Minishell>");
+		add_history(data.input);
+		// data.cmd = ft_split(data.input, ' ');  -> old one
+		parse_cmd(&data);
+		// fill_token_list(&data);
+		exec_cmd(&data);
+		ft_lstclear(&data.token_list, free);
+
+*/
