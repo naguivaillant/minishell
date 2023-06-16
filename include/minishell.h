d1c66743797579284846b8e12bcb93bfaa339fc6
mrabourd <133120742+mrabourd@users.noreply.github.com>
2023-06-16 18:13:16 +0200

    Add files via upload

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mrabourd <mrabourd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:29:02 by mrabourd          #+#    #+#             */
-/*   Updated: 2023/05/21 16:26:56 by mrabourd         ###   ########.fr       */
+/*   Updated: 2023/06/16 13:34:20 by mrabourd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,26 @@ typedef struct s_path
 	char	**tab;
 }				t_path;
 
+typedef struct	s_exec
+{
+	char		**cmd;
+	char		**infile;
+	char		**outfile;
+	int			fdin;
+	int			fdout;
+	int			redirect_input;
+	int			redirect_output;
+	int			heredoc;
+	int			delimiter_append;
+}				t_exec;
+
 typedef struct s_data
 {
+	int			pipes;
 	char		*input;
-	char		**cmd;
+	t_exec		*exec;
+	t_list		*token_list;
+	t_list		*env;
 	t_path		path;
 }				t_data;
 
@@ -49,6 +65,8 @@ int		is_metacharacter(char c);
 int		is_space(char c);
 char	*fill_tmp(char *str, int len);
 void	add_node(t_data *data, char *str, int i, int j);
+void	add_node_double_quote(t_data *data, char *str, int i, int j);
+void	add_node_single_quote(t_data *data, char *str, int i, int j);
 void	split_in_list(t_data *data, char *str);
 
 /* PATH */
@@ -60,19 +78,20 @@ int		count_list(t_list *list);
 void	print_env(t_data *data);
 
 /* EXPORT */
+char	*extract_name(t_data *data, t_list *variable);
 void	ft_sort(t_list *tmp, t_list *print);
 int		builtin_export(t_data *data, t_list *pos);
 
 /* ECHO */
-void	print_lines(int i, char **str, int fd);
-int		mini_echo(t_data *cmd);
+// void	print_lines(int i, char **str, int fd);
+// int		mini_echo(t_data *cmd);
 
 /* UNSET */
-void	builtin_unset(t_data *data);
+void	builtin_unset(t_data *data, t_list *pos);
 
 /* EXIT */
+void	clear_cmd(t_data *data);
 void	free_tab(char **tab);
-void	exit_all(t_data *data, int err);
+void	exit_all(t_data *data, int err, char *str);
 
 #endif
-
